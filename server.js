f3dc5d15ae6ec8939af1118427c8e9cb7bf96654
diptesh17 <deorediptesh1883@gmail.com>
2023-09-05 19:43:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { Socket } = require("socket.io");
+
+const app = express();
+const http = require("http").createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// socket
+
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  console.log("connected...");
+  socket.on("message", (msg) => {
+    socket.broadcast.emit("message", msg);
+  });
+});
